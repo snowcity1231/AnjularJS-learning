00581eaeacfe6f36fada42ff7b6bde98eb52cc3d
snowcity1231 <xuechen911231@aliyun.com>
2017-04-11 14:19:24 +0800

    AngularJS几个简单的demo

---
+++ fast-demo/README.md
@@ -0,0 +1,8 @@
+####AngularJS几个简单的demo，分别表示它的几大特性  
+mvc-demo.html:  MVC三层独立
+  
+module-demo.html:模块化
+  
+directive-demo.html:指令系统  
+
+twoway-binding-demo.html:双向数据绑定
--- fast-demo/directive-demo.js
+++ fast-demo/directive-demo.js
@@ -1,3 +1,11 @@
 /**
  * Created by Administrator on 2017/4/10.
  */
+var myModule = angular.module("myModule", []);
+myModule.directive("hello", function () {
+    return {
+        restrict : 'E',
+        template : '<div>Hi Angular</div>',
+        replace : true
+    }
+});
